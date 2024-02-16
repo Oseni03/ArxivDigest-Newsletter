4154317d6bbb7e92bd1a5434c3f1397f2179a891
Oseni Ayomide Daniel <101524965+Oseni03@users.noreply.github.com>
2024-02-16 17:32:04 +0000

    modify landing-page template

--- src/newsletter/context_processors.py
+++ src/newsletter/context_processors.py
@@ -1,6 +1,8 @@
 from .models import PaperTopic
+from .forms import SubscriberEmailForm
 
 def newsletter(request):
     return {
-        "topics": PaperTopic.objects.parents()
+        "topics": PaperTopic.objects.parents(),
+        "subscription_form": SubscriberEmailForm(),
     }
