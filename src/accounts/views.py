dd2b32f4d1ed61fc34c885ba9a3aea2b7b9a8b8e
Oseni03 <oseniayomide57@gmail.com>
2024-02-19 03:40:59 +0100

    add/modify authentication templates

--- src/accounts/views.py
+++ src/accounts/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 from django.contrib.auth.decorators import login_required
 
@@ -6,3 +6,4 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def logout_view(request):
     logout(request)
+    return redirect("newsletter:home")
